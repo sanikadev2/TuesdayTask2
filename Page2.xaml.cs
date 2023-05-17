f45769200a03730e1deff02f5e17548854569cac
sanikadev2 <117445346+sanikadev2@users.noreply.github.com>
2023-05-17 10:47:27 +0530

    Add files via upload

---
+++ Page2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+
+        int lblValue = 0;
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
---
+++ TabbedPage1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        int lblValue = 0;
+        public TabbedPage1 ()
+        {
+            InitializeComponent();
+        }
+
+        private void Plus_Button_Clicked(object sender, EventArgs e)
+        {
+            lblValue++;
+            lblCount.Text = lblValue.ToString();
+        }
+
+        private void Minus_Button_Clicked(object sender, EventArgs e)
+        {
+            lblValue--;
+            lblCount.Text = lblValue.ToString();
+        }
+    }
+}
